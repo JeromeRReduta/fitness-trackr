e4626af1b1231c20007335d4653ecf9b41b7f732
JRReduta <jrreduta@yahoo.com>
2025-07-21 17:22:08 -0700

    Formatting & refactoring pass

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,32 +1,44 @@
-import { useApi } from "../api/ApiContext";
 import useQuery from "../api/useQuery";
 
 const resource = "/activities";
-let counter = 0;
 
 export default function ActivitiesPage() {
-    console.log(`Times run: ${++counter}: `, useQuery(resource));
     const result = useQuery(resource);
     if (result.loading) {
-        return <h1>Loading, please hold...</h1>;
+        return <Loading />;
     }
     if (result.error) {
-        console.error(result.error);
-        return <h1>Uh oh; something went wrong!</h1>;
+        return <Error error={result.error} />;
     }
     if (!result.data) {
-        /* Checks if data not found yet */
-        return (
-            <>
-                <h1>Activities</h1>
-                <p>Imagine all the activities!</p>
-            </>
-        );
+        return <NoData />;
     }
-    /** Success case: result NOT loading, no error, and data present */
-    const listItems = result.data.map((elem) => (
-        <li key={elem.name}>{elem.name}</li>
-    ));
+    return <ActivityList data={result.data} />;
+}
+
+function Loading() {
+    return <h1>Loading, please hold...</h1>;
+}
+
+function Error({ error }) {
+    console.error(error);
+    return <h1>Uh oh; something went wrong!</h1>;
+}
+
+function NoData() {
+    return (
+        <>
+            <h1>Activities</h1>
+            <p>Imagine all the activities!</p>
+        </>
+    );
+}
+
+function ActivityList({ data }) {
+    if (!data) {
+        console.error("Failed sanity check - undefined data");
+    }
+    const listItems = data.map((elem) => <li key={elem.name}>{elem.name}</li>);
     return (
         <>
             <h1>Activities</h1>
